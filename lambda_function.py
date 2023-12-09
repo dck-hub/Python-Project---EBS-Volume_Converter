a56592785d39d02a4e90e9f37516449ae5a289e3
Daham Kahawearachchi <113774982+dck-hub@users.noreply.github.com>
2023-12-09 11:37:19 +0530

    Create lambda_function.py
    added the lambda_function.py file

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+def get_volumn_id_from_arn(volumn_arn):
+  # split the arn using (':') operater
+    arn_parts = volumn_arn.split(':')   
+    volumn_id = arn_parts[-1].split('/')[-1]
+    return volumn_id
+
+def lambda_handler(event, context):
+    
+    volumn_arn = event['resources'][0]
+    volumn_id  = get_volumn_id_from_arn(volumn_arn)
+    
+    ec2_client = boto3.client("ec2")
+    
+    response = ec2_client.modify_volume(
+      # Get the volume ID and convert the EBS volume into 'gp3' volume type
+        VolumeId = volumn_id,
+        VolumeType = 'gp3'
+    )
+    
